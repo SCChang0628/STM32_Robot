5441d875c4ff8e87fdddb78c93be591e1d061534
Leo <leolamigo0628@gmail.com>
2026-06-18 16:48:13 +0800

    加入雙馬達PWM控制

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body (STM32 original base program)
+  * @brief          : Main program body (Basic dual motor PWM test)
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -13,8 +13,52 @@
 #include "usart.h"
 #include "gpio.h"
 
-/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PV */
+int16_t Motor_PWM = 300;
+/* USER CODE END PV */
+
 void SystemClock_Config(void);
+
+/* USER CODE BEGIN 0 */
+int16_t PWM_Limit(int16_t pwm)
+{
+  if (pwm > 850) return 850;
+  if (pwm < -850) return -850;
+  return pwm;
+}
+
+void Motor_Set(int16_t left_pwm, int16_t right_pwm)
+{
+  left_pwm = PWM_Limit(left_pwm);
+  right_pwm = PWM_Limit(right_pwm);
+
+  if (left_pwm >= 0)
+  {
+    HAL_GPIO_WritePin(AIN1_GPIO_Port, AIN1_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(AIN2_GPIO_Port, AIN2_Pin, GPIO_PIN_RESET);
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, left_pwm);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(AIN1_GPIO_Port, AIN1_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(AIN2_GPIO_Port, AIN2_Pin, GPIO_PIN_SET);
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, -left_pwm);
+  }
+
+  if (right_pwm >= 0)
+  {
+    HAL_GPIO_WritePin(BIN1_GPIO_Port, BIN1_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(BIN2_GPIO_Port, BIN2_Pin, GPIO_PIN_RESET);
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, right_pwm);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(BIN1_GPIO_Port, BIN1_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(BIN2_GPIO_Port, BIN2_Pin, GPIO_PIN_SET);
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, -right_pwm);
+  }
+}
+/* USER CODE END 0 */
 
 int main(void)
 {
@@ -27,9 +71,14 @@ int main(void)
   MX_TIM3_Init();
   MX_I2C1_Init();
 
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_GPIO_WritePin(STBY_GPIO_Port, STBY_Pin, GPIO_PIN_SET);
+
   while (1)
   {
-    HAL_Delay(100);
+    Motor_Set(Motor_PWM, Motor_PWM);
+    HAL_Delay(10);
   }
 }
 
@@ -49,38 +98,3 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                              | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-void Error_Handler(void)
-{
-  __disable_irq();
-  while (1)
-  {
-  }
-}
-
-#ifdef USE_FULL_ASSERT
-void assert_failed(uint8_t *file, uint32_t line)
-{
-}
-#endif
